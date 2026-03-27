3a9e78fdc6ad6dbb042ee85c3fcb29432ffb04d5
Osvin <ogamezm@miumg.edu.gt>
2026-03-26 20:57:17 -0600

    Clase Calculadora

--- src/Encapsulacion/Calculadora.java
+++ src/Encapsulacion/Calculadora.java
@@ -1,5 +1,28 @@
 package Encapsulacion;
 
 public class Calculadora {
+	// Suma
+    public double sumar(double a, double b) {
+        return a + b;
+    }
 
+    // Resta
+    public double restar(double a, double b) {
+        return a - b;
+    }
+
+    // Multiplicacion
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    // Division
+    public double dividir(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: No se puede dividir entre cero.");
+            return 0;
+        }
+        return a / b;
+    }
+	
 }
--- src/Encapsulacion/main.java
+++ src/Encapsulacion/main.java
@@ -105,6 +105,18 @@ public class main {
         
         System.out.println("\n		EJERCICIO 6: CLASE CALCULADORA");
         
-        
+        Calculadora calc = new Calculadora();
+        double n1 = 15.0;
+        double n2 = 4.0;
+        double nCero = 0.0;
+
+        System.out.println("Operaciones con " + n1 + " y " + n2 + ":");
+        System.out.println("Suma: " + calc.sumar(n1, n2));
+        System.out.println("Resta: " + calc.restar(n1, n2));
+        System.out.println("Multiplicación: " + calc.multiplicar(n1, n2));
+        System.out.println("División: " + calc.dividir(n1, n2));
+
+        System.out.println("\nIntentando dividir " + n1 + " entre " + nCero + ":");
+        double resultadoError = calc.dividir(n1, nCero);
 	}
 }
